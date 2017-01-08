bdaf877f6ce40087938e42aaf6556a79bb8cc502
dperry233 <freemail40@yahoo.com>
2017-01-08 21:22:23 +0200

    change to AVLTREE with pointers

--- avlTree.h
+++ avlTree.h
@@ -494,6 +494,7 @@ TreeResult AVLTree<keyType, dataType>::removeValue (keyType & iKey) {
     }*/
     AVLNode<keyType, dataType> * parent = NULL;
     AVLNode<keyType, dataType> * tmp = rootNode->returnNode(iKey);
+    AVLNode<keyType, dataType> * replace = NULL;
     if (tmp->father) {
         parent = tmp->father;
     }
@@ -554,22 +555,69 @@ TreeResult AVLTree<keyType, dataType>::removeValue (keyType & iKey) {
             delete tmp;
         }
     } else {
-        AVLNode<keyType, dataType> * replace = tmp->rightSon;
+        replace = tmp->rightSon;
         while (replace->leftSon) {
             replace = replace->leftSon;
         }
-        tmp->swap(replace);
-        parent = replace->father;
+        //tmp->swap(replace); TODO: We swap pointers instead of doing a value and key swap
+        // replace is the following node
+        // tmp is the one to be deleted
+        AVLNode<keyType, dataType> * swap;
+        swap = tmp->father;
+        tmp->father = replace->father;
+        replace->father = swap;
+        if (tmp->father == tmp) {
+            (tmp->father = replace);
+        }
+
+        swap = tmp->leftSon;
+        tmp->leftSon = replace->leftSon;
+        replace->leftSon = swap;
+
+        swap = tmp->rightSon;
+        tmp->rightSon = replace->rightSon;
+        replace->rightSon = swap;
+        if (replace->rightSon == replace) { // if switching between father and right son
+            replace->rightSon = tmp;
+        }// now replace is in the right place but his friends don't know yet
+        // and tmp is in the place of his follower and needs to be deleted
+
+        if (replace->father) {
+            if (replace->father->rightSon->key == tmp->key) { // this guy is the right son because temp was the right
+                replace->father->rightSon = replace;
+            } else replace->father->leftSon = replace;
+        }
+
+        if (replace->rightSon) {
+            replace->rightSon->father = replace;
+        }
+
+        if (replace->leftSon) {
+            replace->leftSon->father = replace;
+        }
+
+//        if (tmp->father) {
+//            if (tmp->father->rightSon->key == replace->key) { // to be deleted is the right son
+//                tmp->father->rightSon = tmp;
+//            } else tmp->father->leftSon = tmp;
+//        }
+
+        if (tmp->rightSon) {
+            tmp->rightSon->father = tmp;
+        }
+
+        parent = tmp->father;
         if (parent->rightSon) {
-            if (parent->rightSon->key == replace->key) {
-                parent->setRightSon(replace->rightSon);
+            if (parent->rightSon->key == tmp->key) {
+                parent->setRightSon(tmp->rightSon);
             } else {
-                parent->setLeftSon(replace->rightSon);
+                parent->setLeftSon(tmp->rightSon);
             }
         } else {
-            parent->setLeftSon(replace->rightSon);
+            parent->setLeftSon(tmp->rightSon);
         }
-        delete replace;
+
+        delete tmp;
         while (parent) {
             parent->getAndSetHeight();
             parent->updateBalanceFactor();
@@ -578,7 +626,9 @@ TreeResult AVLTree<keyType, dataType>::removeValue (keyType & iKey) {
         }
     }
     size--;
-    //if (0==size) rootNode=NULL; // TODO: Check if this works instead of doing something different for size=1
+    if (replace != NULL && !replace->father) {
+        this->rootNode = replace;
+    }
     return AVLTREE_SUCCESS;
 }
 
@@ -622,8 +672,8 @@ TreeResult buildFullTree (AVLTree<keyType, dataType> * tree, AVLNode<keyType, da
         root = new AVLNode<keyType, dataType>(keyType(), dataType());
         if (NULL != parent) {
             root->father = parent;
-            if (LEFT == direction) root->father->leftSon=root;
-            else root->father->rightSon=root;
+            if (LEFT == direction) root->father->leftSon = root;
+            else root->father->rightSon = root;
         } else { // this is the first node!
             tree->rootNode = root;
         }
